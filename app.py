cea95be269ab3a838e2b623034498c56080a910b
Raphael Gilliot <rgilliot@enseirb-matmeca.fr>
2022-03-10 14:49:37 +0100

    Added basic visualization
    The app now allows visualization of a few sequences in dropdown menus

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dash import Dash
 import pandas as pd
 
 
-data = pd.read_csv(".\events.csv")
+data = pd.read_csv(".\events.csv    ")
 
 #quick loop to only select a couple sequences
 seq1 = []
@@ -28,14 +28,19 @@ seq2.sort(key=lambda x:x[0])
 seq3.sort(key=lambda x:x[0])
 
 venues = list(set(data['Venue']))
+external_stylesheets = [{"rel":"stylesheet"}]
 
-app = Dash(__name__)
+app = Dash(__name__,external_stylesheets=external_stylesheets)
 
+sequences = [seq1,seq2,seq3]
 
-app.layout = html.Div([
-    dcc.Dropdown(venues, event[1]) for event in seq2
-
-])
+app.layout = html.Div(
+    [html.Div(
+        [dcc.Dropdown(venues, event[1],className="event") for event in seq],
+        className = "sequence"
+    )
+    for seq in sequences]
+)
 
 ####separation
 if __name__=='__main__':
