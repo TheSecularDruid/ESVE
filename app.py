59bfc910fd79a5af5921b9728af32141526baf2c
Raphael Gilliot <rgilliot@enseirb-matmeca.fr>
2022-03-23 10:07:36 +0100

    Changed app direction toward single sequence complex visualization
    updated dataloading to reflect that
    also optimized the routine and dropped the panda usage

--- app.py
+++ app.py
@@ -1,51 +1,69 @@
 from dash import html
 from dash import dcc
 from dash import Dash
-import pandas as pd
+import csv
 
 
-data = pd.read_csv(".\events.csv    ")
 
-#quick loop to only select a couple sequences
-n_attr = len(data.columns) - 2 #removing ID and time attribute for now
-
-n_seq = 5
-sequences = [[-1,[]] for _ in range(n_seq)]   #First element is sequence ID, second element is a list of events
-
-index = 0
-for seq in sequences:
-    if data['ID'][index] not in [s[0] for s in sequences]:
-        seq[0] = data['ID'][index]
-    index+=1
-
-for index in range(data.shape[0]):
-    for seq in sequences:
-        if data['ID'][index]==seq[0]:
-            seq[1].append([data[attr][index] for attr in data.columns[1:]])
+##Data loading
+sequences = {}
+attr_values = {}
 
 
-for seq in sequences:
-    seq[1].sort(key=lambda x:x[0])
+with open("./events.csv",'r') as f:
+    r = csv.reader(f,csv.QUOTE_NONNUMERIC)
+    attributes = r.__next__()
+    for attr in attributes:
+        attr_values[attr] = []
 
-attributes_values = [list(set(data[attr])) for attr in data.columns[1:]]
-n_attributes = len(attributes_values)
+    for row in r:
+        if row[0] not in sequences.keys():
+            sequences[row[0]] = []
+        sequences[row[0]].append(row[1:])
+        for val_id in range(2,len(row)):
+            if row[val_id] not in attr_values[attributes[val_id]]:
+                attr_values[attributes[val_id]].append(row[val_id])
+
+
+
+for seq in sequences.values():
+    seq.sort(key=lambda x:x[0])
+
+cur_seq_ID = list(sequences.keys())[0]
+
+attr_types = {}
+for attr in attributes:
+    attr_types[attr] = 'Categorical'
+
+
+##App setup
 external_stylesheets = [{"rel":"stylesheet"}]
-
 app = Dash(__name__,external_stylesheets=external_stylesheets)
 
 
-venues = attributes_values[1]
+# app.layout = html.Div(
+#     dcc.Dropdown(sequences.keys(),cur_seq_ID,id='sequence_chooser'),
+#     html.Div([
+#             html.Div(
+#                 [dcc.Dropdown(
+#                     attr_values[attr],
+#                     sequences[cur_seq_ID][event_index],
+#                     className='event')
+#                 for event_index in range(len(sequences[cur_seq_ID]))],
+#                 className='sequence')
+#         for attr_index,attr_type in enumerate(attr_types.values())]
+#
+#     )
+# )
+#
+#
+# [[(attr_values[attr],sequences[cur_seq_ID][event_index])
+#                 for event_index in range(len(sequences[cur_seq_ID]))]
+#         for attr_index,attr_type in enumerate(attr_types.values())]
+#
 
-app.layout = html.Div(
-    [html.Div(
-        [dcc.Dropdown(venues, event[k],className="event")
-            for k in range(n_attributes)
-        for event in seq[1]],
-        className = "sequence"
-    )
-    for seq in sequences]
-)
 
-####separation
+
+####Run
 if __name__=='__main__':
     app.run_server(debug=True)
