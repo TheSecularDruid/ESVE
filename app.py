d6345244251ba9e6b396440a12299bcca06a0e6e
Raphael Gilliot <rgilliot@enseirb-matmeca.fr>
2022-03-25 16:14:43 +0100

    Added graphs
    Addded support for Numerical attributes so they're represented as graphs instead of dropdown

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from dash import html
 from dash import dcc
 from dash import Dash
+from dash import Input
+from dash import Output
+from plotly import express as px
+import pandas as pd
 import csv
 
 
@@ -29,18 +33,22 @@ with open("./events.csv",'r') as f:
 for seq in sequences.values():
     seq.sort(key=lambda x:x[0])
 
-cur_seq_ID = list(sequences.keys())[0]
+cur_seq_ID = list(sequences.keys())[17]
 
 attr_types = {}
 for attr in attributes:
     attr_types[attr] = 'Categorical'
 
 
+#temporary
+attr_types['Price'] = 'Numerical'
+
 ##Display
 external_stylesheets = [{"rel":"stylesheet"}]
 app = Dash(__name__,external_stylesheets=external_stylesheets)
 
-def dropdown_list_or_graph(attr_index):
+def dropdown_list_or_graph(attr_index,cur_seq_ID):
+    attr = attributes[attr_index]
     if attr_types[attr]=='Categorical':
         DDList = []
         for event in sequences[cur_seq_ID]:
@@ -50,11 +58,15 @@ def dropdown_list_or_graph(attr_index):
             DDList.append(dcc.Dropdown(options=options,value=value))
         return DDList
     if attr_types[attr]=='Numerical':
-        pass
-
+        values = dict({
+            'Time':[event[0] for event in sequences[cur_seq_ID]],
+            'Value':[int(event[attr_index-1]) for event in sequences[cur_seq_ID]]
+            })
+        fig = px.line(values,x='Time',y='Value')
+        return dcc.Graph(figure=fig)
 
 def horizontal_box(attr_index):
-    return html.Div(children=dropdown_list_or_graph(attr_index),className = 'horizontal_box')
+    return html.Div(children=dropdown_list_or_graph(attr_index,cur_seq_ID),className = 'horizontal_box')
 
 vertical_box = html.Div(children=[horizontal_box(attr_index) for attr_index in range(2,len(attributes))],#not displaying time, ID
                         className = 'vertical_box')
@@ -70,7 +82,13 @@ app.layout = html.Div(children = [
 
 
 ##Callbacks
-#TODO
+# @app.callback(
+#     Output(component_id='this', component_property='children'),
+#     Input(component_id='sequence_chooser', component_property='value')
+# )
+# def cur_seq(input_value):
+#     cur_seq_ID=input_value
+#     return(dropdown_list_or_graph(attr_index,cur_seq_ID))
 
 ####Run
 if __name__=='__main__':
